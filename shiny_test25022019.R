448a8c926f309f4d304c350ce65ce11eb083a6a9
yishu-lin <47976804+yishu-lin@users.noreply.github.com>
2019-03-08 14:15:34 +0000

    Add files via upload
    Test version

---
+++ shiny_test25022019.R
@@ -0,0 +1,266 @@
+
+
+
+# for testing the package, Shiny
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
+
+#install.packages("shiny")
+#install.packages('rsconnect')
+#install.packages('Cairo')
+
+library(shiny)
+library(rsconnect)
+library(Cairo)
+
+
+
+
+#Import the required data from the data sheets created by the Excel file
+LifeTable			=data.frame(read.table(file="InputFiles/LifeTable.txt",    			header=T))
+Incidence			=data.frame(read.table(file="InputFiles/Incidence.txt",      		header=T))
+TestPerformance		=data.frame(read.table(file="InputFiles/TestPerformance.txt",		header=T))
+PreclinicalDuration	=data.frame(read.table(file="InputFiles/PreclinicalDuration.txt",	header=T))
+ScreenSchedule		=data.frame(read.table(file="InputFiles/ScreenSchedule.txt",		header=T))
+TreatmentSuccess	=data.frame(read.table(file="InputFiles/TreatmentSuccess.txt",		header=T))
+DiscountRates		=data.frame(read.table(file="InputFiles/DiscountRates.txt",			header=T))
+Costs				=data.frame(read.table(file="InputFiles/Costs.txt",					header=T))
+source(file="InputFiles/Misc.txt")
+source(file="InputFiles/Options.txt")
+#Import the results of OWSA to the ICERTable
+ICERTable			=data.frame(read.table(file="OutputFiles/LatestResults/ICERTable2019-02-28.txt",    			header=T))
+
+NumberofSegments=19
+strategies <- paste(ScreenSchedule$StartAge,ScreenSchedule$StopAge,ScreenSchedule$Interval1,ScreenSchedule$TestApplied1,sep="_")
+strategies <- strategies[!(strategies %in% "20_70_1_1")]
+NumberOfStrategies=length(strategies)
+
+
+ui<-fluidPage(
+  title="Cost-effectiveness Analysis",
+  fluidRow(
+    column(3,
+           h2("Cost-effectiveness Analysis"),           
+           h3("(One-Way Sensitivity Analysis)"),
+           radioButtons("parameters", label = "Parameters", choices = list(
+             "Test Sensitivity" =  "TestSensitivity1" ,                 
+            # "Test Sensitivity 2" = "TestSensitivity2" ,                
+            # "Test Sensitivity 3" = "TestSensitivity3" ,                 
+            # "Test Sensitivity 4" = "TestSensitivity4" ,                
+            # "Test Sensitivity 5" = "TestSensitivity5" ,                 
+             "Test Specificity" = "TestSpecificity1" ,                
+            # "Test Specificity 2" = "TestSpecificity2" ,                 
+            # "Test Specificity 3" = "TestSpecificity3" ,                
+            # "Test Specificity 4" = "TestSpecificity4" ,                 
+            # "Test Specificity 5" = "TestSpecificity5" ,               
+             "Discount Rate - Costs" = "DiscountRateCosts" ,    
+             "Discount Rate - Effects" = "DiscountRateEffects" ,                
+             "Discount Year" = "DiscountYear" ,            
+             "PreClinical Probability" = "PreClinicalProbability" , 
+             "Clinical Probability" = "ClinicalProbability" ,     
+             "Cost of Primary Screen" = "CostofPrimaryScreen" ,          
+             "Cost of FollowUp "= "CostofFollowUp" ,                
+             "Cost of Treatment (Detected by Screens)"= "CostofTreatScreenDetected" ,
+             "Cost of Treatment Clinical"= "CostofTreatmentClinical",
+             "Incidence Rate" = "Incidence",
+             "Preclinical Duration" = "PreclinicalDuration")
+            ),           
+           hr(),           
+           sliderInput("slider1", label = "Parameter Value", min = 1, max = (NumberofSegments+1), value=0, step = 1),
+          # br(),
+           sliderInput("slider2", label = "Threshold", min = 10000, max = 200000, value=100000, step = 5000)
+    ),
+    column(8,
+           plotOutput("distPlot", click = "plot_click"),
+           hr()
+    ),
+    column(8,offset = 1,
+           h4("The Strategy under Oberservation"),
+           sliderInput("startage", label = "Screen Startage", min = 20, max = 70, value=30, step = 5),
+           sliderInput("stopage", label = "Screen Stopage", min = 50, max = 70, value=70, step = 5),
+           sliderInput("interval", label = "Screen Interval", min = 1, max = 10, value=1, step = 1),
+           hr(),
+           verbatimTextOutput("a"),
+           verbatimTextOutput("info")
+   )
+  )
+)
+
+
+
+
+
+
+
+
+
+
+
+server<-shinyServer(function(input, output) {
+  output$distPlot <- renderPlot({
+  
+   #**************************************************ONE-WAY SENSITIVITY ANALYSES (OWSA)  END************************************************************************************************
+
+
+    
+      #Define the scenario we are going to plot on the CE plane:as.numeric(input$slider1)
+      #Define the threshold
+      Scenario<- which(rownames(ICERTable)==paste(input$parameters,input$slider1,sep="_"))
+      Threshold<-input$slider2
+      ObsearvationPoint<- paste(input$startage,"_",input$stopage,"_",input$interval,"_",1,sep="")
+    
+      
+      
+      
+      PlotData<-array(0,dim=c(NumberOfStrategies,3))
+      rownames(PlotData)<- strategies
+      colnames(PlotData)<- c("D_Costs","D_Effects","ICER")
+      
+      #source("C:/Users/yilin/Downloads/DARTH workshop/decision modeling using r/Day 1/Functions/CEA_functions.R")
+      ### Get CEA frontier 
+      #ce.mat <- cbind(Strategy = 1:20, 
+      #                Cost = as.numeric(ICERTable[Scenario,][colnames(ICERTable) %in% paste("D_Costs",strategies,sep="_")])/10^6, 
+      #                Effectiveness = as.numeric(ICERTable[Scenario,][colnames(ICERTable) %in% paste("D_Effects",strategies,sep="_")]))
+      #ce.front <- getFrontier(ce.mat,maxWTP = Threshold, plot = TRUE)
+      
+      
+      #Define the costs and effects; costs here expressed in millions
+      PlotData[,"D_Costs"]<-as.numeric(ICERTable[Scenario,][colnames(ICERTable) %in% paste("D_Costs",strategies,sep="_")])/10^6
+      PlotData[,"D_Effects"]<-as.numeric(ICERTable[Scenario,][colnames(ICERTable) %in% paste("D_Effects",strategies,sep="_")])
+      PlotData[,"ICER"]<-as.character(unlist(ICERTable[Scenario,][colnames(ICERTable) %in% paste("ICER",strategies,sep="_")]))
+      
+      
+      SameParameterD_Costs<-ICERTable[c(rownames(ICERTable) %in% paste((strsplit(paste(rownames(ICERTable)),split="_")[[Scenario]][1]),1:(NumberofSegments+1),sep="_")),][colnames(ICERTable) %in% paste("D_Costs",strategies,sep="_")]
+      SameParameterD_Effects<-ICERTable[c(rownames(ICERTable) %in% paste((strsplit(paste(rownames(ICERTable)),split="_")[[Scenario]][1]),1:(NumberofSegments+1),sep="_")),][colnames(ICERTable) %in% paste("D_Effects",strategies,sep="_")]
+      
+      
+      if (round(max(SameParameterD_Costs),-3)-max(SameParameterD_Costs)>=0)
+      {MaxCost=round(max(SameParameterD_Costs)/10^6,-3)} else {MaxCost=round(max(SameParameterD_Costs)/10^6,-3)+500}
+      
+      if (round(max(SameParameterD_Effects),-3)-max(SameParameterD_Effects)>=0)
+      {MaxEffect=round(max(SameParameterD_Effects),-3)} else {MaxEffect=round(max(SameParameterD_Effects),-3)+1000}
+      
+      
+      
+      #*****************************************  To Plot the CE plane*********************************************************************************************************
+      #Plot the CE plane
+      Effects				=PlotData[,"D_Effects"]
+      Costs				=PlotData[,"D_Costs"]
+      
+      
+      #BoundarySet is the strategies whose ICER is not "SD" or "ED"
+      BoundarySet<- PlotData[which(!(PlotData[,"ICER"]%in%c("SD","ED"))),]
+      if (length(which(!(PlotData[,"ICER"]%in%c("SD","ED"))))==1){}else{
+        
+        #Do the same for the efficient frontier
+        {
+          EfficientEffects	=BoundarySet[,"D_Effects"]
+          EfficientCosts		=BoundarySet[,"D_Costs"]}
+        
+        #OptimalStrategy:
+        #Identify the highest of the cost-effective ICERs given the threshold
+        BoundarySetWithoutRef<-BoundarySet[which(BoundarySet[,"ICER"]!=c("reference")),]
+        
+        if (min(as.numeric(BoundarySetWithoutRef[,"ICER"]))<=Threshold){
+          OptimalStrategy<-rownames(BoundarySetWithoutRef)[which(BoundarySetWithoutRef[,"ICER"]==(max(as.numeric(BoundarySetWithoutRef[,"ICER"])[as.numeric(BoundarySetWithoutRef[,"ICER"])<=Threshold],na.rm=TRUE)))]
+          CostEffectiveICER=BoundarySetWithoutRef[OptimalStrategy,"ICER"]
+          #Find the corresponding costs and effect
+          CostEffectiveEffects=BoundarySetWithoutRef[OptimalStrategy,"D_Effects"]
+          CostEffectiveCosts	=BoundarySetWithoutRef[OptimalStrategy,"D_Costs"]
+        }else{
+          CostEffectiveICER=c("reference")
+          CostEffectiveEffects=BoundarySet[,"D_Effects"][which(BoundarySet[,"ICER"]=="reference")]
+          CostEffectiveCosts=BoundarySet[,"D_Costs"][which(BoundarySet[,"ICER"]=="reference")]
+          
+        }
+        
+        
+        
+        
+        
+        
+        #Set the tick marks to incude zero and any big number(here are 10,000 and 2,000, separately)
+        TicksEffects		=pretty(c(0,MaxEffect	))
+        TicksCosts			=pretty(c(0,MaxCost	))
+        
+        
+        
+        #Set the ranges for both values
+        xRange=range(TicksEffects)
+        yRange=range(TicksCosts	 )
+        
+        #Plot the plane
+        #Set the margin parameters
+        par(mar=c(5, 6, 4, 1))
+        plot(Effects,Costs,yaxt="n",xaxt="n", xlab="",ylab="", xlim=xRange,ylim=yRange, cex=1)
+        #Add the axes
+        axis(1, at = TicksEffects,	labels = formatC(TicksEffects,  big.mark = ",", format = "d"),las = 1)
+        axis(2, at = TicksCosts	 , 	labels = formatC(TicksCosts,	big.mark = ",", format = "d"),las = 1)
+        #Add in the X and Y axis labels with spacing
+        title(paste("Scenario",strsplit(paste(rownames(ICERTable)[Scenario]),split="_")[[1]][2],":",strsplit(paste(rownames(ICERTable)[Scenario]),split="_")[[1]][1],"=",sprintf("%.3f",as.numeric(ICERTable[Scenario,paste(strsplit(paste(rownames(ICERTable)[Scenario]),split="_")[[1]][1])]),sep=" ")),ylab="Costs (£M)", xlab="Effects, LYG", mgp=c(3.75,1.75,0), cex.lab=1.2)
+
+        
+        #Add the efficient frontier
+        sequence=order(as.numeric(EfficientEffects))
+        lines (EfficientEffects[sequence],EfficientCosts[sequence],lwd=2)
+        points(EfficientEffects,EfficientCosts,pch=19,lwd=2)
+        #Add the optimal strategy
+        points(CostEffectiveEffects,CostEffectiveCosts,pch=19,lwd=3,cex=1,col="forestgreen")
+        #Add a legend
+        legend("topright",legend=c(paste("The Optimal Strategy",paste(ifelse(OptimalStrategy %in% "NA","NA",OptimalStrategy)))),pch=19,bty="o",pt.cex =1,col="forestgreen", cex=0.8)
+        #Add a line to show the threshold
+        abline(a=as.numeric(CostEffectiveCosts)-as.numeric(CostEffectiveEffects)*Threshold/10^6,b=Threshold/10^6,         col="red", lwd=3, lty=2)
+        #Add the specific strategy that we want to observe
+        points(PlotData[ObsearvationPoint,"D_Effects"],PlotData[ObsearvationPoint,"D_Costs"],pch=19,lwd=3,cex=1,col="red")
+        #Add a legend
+        #legend("topright",legend=c(paste("The Obsearved Strategy",paste(ifelse(ObsearvationPoint %in% "NA","NA",ObsearvationPoint)))),pch=19,bty="o",pt.cex =1,col="red", cex=0.8)
+        
+        #Create ICER labels: only when the strategy's "D_Effects" is higher than or equal to 4,000
+        ICERLabels=gsub("NA","",noquote(format(c(BoundarySet[which(BoundarySet[,"D_Effects"]>=4000),"ICER"],CostEffectiveICER),big.mark=",")))
+        
+        #Add the ICERs to the frontier
+        #Generate a label offset to place the values at an offset
+        LabelYOffset=max(as.numeric(EfficientCosts))/40
+        #Apply the text
+        text(c(EfficientEffects[which(BoundarySet[,"D_Effects"]>=4000)],CostEffectiveEffects),c(as.numeric(EfficientCosts)[which(BoundarySet[,"D_Effects"]>=4000)],as.numeric(CostEffectiveCosts))-LabelYOffset,labels=prettyNum(ICERLabels,big.mark=",",scientific=FALSE), pos=4)
+        
+        
+       
+      }
+    
+  })
+  
+    
+    #renderText({    
+    
+    #Define the scenario we are going to plot on the CE plane:as.numeric(input$slider1)
+    #Define the threshold
+  output$a <- renderText({
+         Scenario<- which(rownames(ICERTable)==paste(input$parameters,input$slider1,sep="_"))
+         Threshold<-input$slider2
+         
+         
+         
+         PlotData<-array(0,dim=c(NumberOfStrategies,3))
+         rownames(PlotData)<- strategies
+         colnames(PlotData)<- c("D_Costs","D_Effects","ICER")
+         
+         
+         #Define the costs and effects; costs here expressed in millions
+         PlotData[,"D_Costs"]<-as.numeric(ICERTable[Scenario,][colnames(ICERTable) %in% paste("D_Costs",strategies,sep="_")])/10^6
+         PlotData[,"D_Effects"]<-as.numeric(ICERTable[Scenario,][colnames(ICERTable) %in% paste("D_Effects",strategies,sep="_")])
+         PlotData[,"ICER"]<-as.character(unlist(ICERTable[Scenario,][colnames(ICERTable) %in% paste("ICER",strategies,sep="_")]))
+         
+      paste(nearPoints(PlotData,input$plot_click,xvar="D_Effects",yvar="D_Costs",threshold = 5000,addDist=TRUE))
+    })
+     
+  output$info <- renderPrint({  
+         
+    paste0("Life-year gained: ", input$plot_click$x, "\nCost:£ ", input$plot_click$y, "  (million)","\nStrategy: ")
+    #,rownames(PlotData)[PlotData[,"D_Effects"] %in% paste(input$plot_click$x)]
+#,isolate(reactive({as.character(output$a)}))
+    #nearPoints(PlotData,input$plot_click,xvar="D_Effects",yvar="D_Costs",panelvar1="D_Effects",panelvar2="D_Costs",threshold = 5000,addDist=TRUE)
+  })
+})
+
+shinyApp(ui=ui,server=server)
+
